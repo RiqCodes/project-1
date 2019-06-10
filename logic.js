c209414ba39f6210934a5ccf6800d630b10df071
Denzel <denzelhartshorn@gmail.com>
2019-06-10 15:33:30 -0700

    updated favs page & improved functionality

--- logic.js
+++ logic.js
@@ -9,7 +9,39 @@ document.addEventListener('DOMContentLoaded', function() {
   $(document).ready(function(){
     $('.sidenav').sidenav();
   
-    
+    $(".card-action").on("click", function() {
+        // $()append("collection-item avatar")
+        // get the user input from form
+        const userLocation  = $("#icon_prefix").val().trim();
+        const endLocation = $("#icon_telephone").val().trim(); 
+        
+        // create new item for the unorder list
+        const newListItem = $("<li>");
+        newListItem.addClass("collection-item avatar");
+        const $pTag = $("<p>");
+        const $span = $("<span>");
+        $span.text("Title")
+        // const $img = $("<img>");
+        const $ifolder = $("<i>")
+        $ifolder.addClass("material-icons circle")
+        $ifolder.text("folder")
+        // $img.attr("src","")
+        const $a = $("<a>"); 
+        $a.addClass("secondary-content")
+        $a.attr("href", "#")
+        const $i = $("<i>")
+        $i.addClass("material-icons")
+        $i.text("grade")
+        $a.append($i); 
+        // $img.addClass("circle");
+        $span.addClass("title");
+        $pTag.html(userLocation + "<br>" + endLocation);
+        // append img, span, a tag
+        newListItem.append($ifolder, $span, $pTag, $a);
+
+        // append new item to order list
+        $(".collection").append(newListItem); 
+    })
 
 });
 
