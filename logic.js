672496ec5ab9d720b1ced695ec81c27030dab559
Denzel <denzelhartshorn@gmail.com>
2019-06-10 13:03:15 -0700

    working navbar

--- logic.js
+++ logic.js
@@ -1,7 +1,16 @@
-$(document).ready(function(){
-    $.ajax({
-        
-        
-    })
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    // var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  
+    
 
 });
+
+  
