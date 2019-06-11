4307900e682a7fda59337885e6fe1e50288a57e0
Michael's <mjlikre@gmail.com>
2019-06-10 17:04:59 -0700

    logic almost done, still need to figure out how to display routes from individual stops and how to show users how to get to each stop. Preferably would need to add in distance from user's position to the stop. Still need to do time conversion, etc

--- logic.js
+++ logic.js
@@ -1,38 +1,80 @@
-$(document).ready(function(){
-    
-    
-    var distance= 500;
-    var routeName = '51b';
-   
+    $(document).ready(function(){
+        
+        
+        var IP;
+        var latitude;
+        var longitude;
+        var stopID; 
 
-    $.ajax({
-        url : 'https://api.ipify.org?format=jsonp&callback=?',
-        dataType : "json"
-    }).then(function(data){
-        var IP = data.ip
-        var query = {
-            url : `http://api.ipstack.com/${IP}?access_key=711ae091724cdd59c84aed29e5d6d3d0`,
-            method: "GET"
+        function getIP(){
+            $.ajax({
+                url : 'https://api.ipify.org?format=jsonp&callback=?',
+                dataType : "json"
+            }).then(function(data){
+                IP = data.ip
+                getLocation()
+            });
+
         }
-        console.log(data)
-        
-        
-        
-        
-        $.ajax(query).then(function(location){
+        function getLocation(){
+            var query = {
+                url : `http://api.ipstack.com/${IP}?access_key=711ae091724cdd59c84aed29e5d6d3d0`,
+                method: "GET"
+            }
+            console.log(IP);
             
-            console.log(location);
-            var latitude = location.latitude;
-            var longitude = location.longitude; 
-            var $query = `http://api.actransit.org/transit/stops/${latitude}/${longitude}/1000/?token=73C0EC914517EE7D0DA47B8BE90D788B`;
-            console.log(latitude)
+            $.ajax(query).then(function(location){
+                
+                
+                latitude = location.latitude;
+                longitude = location.longitude;
+                getStops(); 
+            });
+            
+
+        }
+
+        function getStops(){
+            var $query = `http://api.actransit.org/transit/stops/${latitude}/${longitude}/10000/?token=73C0EC914517EE7D0DA47B8BE90D788B`;
+        
             $.ajax({
                 url : $query,
                 method: "GET"
-            }).then(function(bus){
-                console.log(bus);
+            }).then(function(stops){ 
+                console.log(stops)
+                for (var stop = 0; stop < Object.keys(stops).length; stop ++){
+                    var stopbtn = $('<button>').attr("id", 'stops');
+                    stopbtn.attr('stopID', stops[stop].StopId).text(stops[stop].Name);
+                    $('#display').append(stopbtn);
+                }
             })
-        })
-    });  
+            
+        }
+        getIP();
+        function getVehicleOnStop(){
+            $.ajax({
+                url : `https://api.actransit.org/transit/stops/${stopID}/predictions/?token=73C0EC914517EE7D0DA47B8BE90D788B`,
+                method : "GET"
+            }).then(function(predictions){
+                console.log(predictions);
 
-});
+            })
+
+
+        }
+
+        $(document).on('click', '#stops', function(){
+            stopID = $(this).attr('stopID');
+            getVehicleOnStop();
+
+        });
+
+        
+        
+        
+    
+    
+    
+    });
+
+    // need to add in display section, 
