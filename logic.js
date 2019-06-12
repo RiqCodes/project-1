b9b3fe000ce60a9871ae065fc7175c29c2f8503b
Denzel <denzelhartshorn@gmail.com>
2019-06-12 11:18:55 -0700

    firebase updated & streamlined

--- logic.js
+++ logic.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     // var instances = M.Sidenav.init(elems, options);
@@ -5,8 +6,86 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
   // Or with jQuery
+  let favArray = []
+
+  function Fav(start, end) {
+      this.startLocation = start
+      this.endLocation = end
+  }
+  
+//   database.ref().once('value', function (snap) {
+//       console.log(snap.val().faves)
+//       favArray = snap.val().faves
+  
+  
+//       for(let i = 0; i < favArray.length; i++) {
+//            // create new list item for the unorder list
+//         const newListItem = $("<li>");
+//         // add a class to the <li> 
+//         newListItem.addClass("collection-item avatar");
+//         // creating a p tag
+//         const $pTag = $("<p>");
+//         // creating a span tag
+//         const $span = $("<span>");
+//         //set the text in span to Title
+//         $span.text("Title");
+//         // gives a class to the span 
+//         $span.addClass("title");
+//         // const $img = $("<img>");
+//         // $img.attr("src","")
+//         // creating a icon tag
+//         // $img.addClass("circle");
+//         const $ifolder = $("<i>")
+//         // adding a class to the icon tag
+//         $ifolder.addClass("material-icons circle")
+//         // gives text the icon tag
+//         $ifolder.text("folder")
+//         // creating an anchor tag
+//         const $a = $("<a>"); 
+//         // add a class to the anchor tag
+//         $a.addClass("secondary-content")
+//         // gives the anchor tag a link 
+//         $a.attr("href", "#")
+//         // creates another icon tag
+//         const $i = $("<i>")
+//         // gives a class to the second icon tag
+//         $i.addClass("material-icons")
+//         // gives text to the icon tag
+//         $i.text("grade")
+//         // apend the icon tag to the anchor tag
+//         $a.append($i); 
+//         // give userLocation and endLocation tot the p tag
+//         $pTag.html(userLocation + "<br>" + endLocation);
+//         // append $ifolder, $span, $a tag
+//         newListItem.append($ifolder, $span, $pTag, $a);
+//         // append new item to order list
+//         $(".collection").append(newListItem); 
+//       }
+//   })
+  
+
+
+  let database
 
   $(document).ready(function(){
+
+    if (window.firebase) {
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyCi-pMxoEWX4y7lvwYuREsjX3LEqnegu3w",
+            authDomain: "denzel-s-uniquer-project-1.firebaseapp.com",
+            databaseURL: "https://denzel-s-uniquer-project-1.firebaseio.com",
+            projectId: "denzel-s-uniquer-project-1",
+            storageBucket: "denzel-s-uniquer-project-1.appspot.com",
+            messagingSenderId: "169352910796",
+            appId: "1:169352910796:web:0fbbd3cf4c0b6c15"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+        database = firebase.database()
+    }
+
     $('.sidenav').sidenav();
 
     // select option
@@ -19,9 +98,85 @@ document.addEventListener('DOMContentLoaded', function() {
     $(".card-action").on("click", function() {
         // $()append("collection-item avatar")
         // get the user input from form
-        const userLocation  = $("#start").val().trim();
-        const endLocation = $("#end").val().trim(); 
+        const startLocation  = $("#start").val().trim();
+        const endLocation = $("#end").val().trim();
         
+        const userCoordinates = new Fav(startLocation, endLocation)
+
+        // favArray.push(userCoordinates)
+
+        // database.ref().set({
+        //     faves: favArray
+        // });
+
+        // database.ref().child("favorites").push( userCoordinates )
+
+        const key = database.ref().child("favorites").push().key
+        const updates = {}
+        updates[`/favorites/${key}`] = userCoordinates
+        console.log(updates)
+        database.ref().update(updates)
+
+        renderFavorites(key, startLocation, endLocation)
+        // // create new list item for the unorder list
+        // const newListItem = $("<li>");
+        // // add a class to the <li> 
+        // newListItem.addClass("collection-item avatar");
+        // // creating a p tag
+        // const $pTag = $("<p>");
+        // // creating a span tag
+        // const $span = $("<span>");
+        // //set the text in span to Title
+        // $span.text("Title");
+        // // gives a class to the span 
+        // $span.addClass("title");
+        // // const $img = $("<img>");
+        // // $img.attr("src","")
+        // // creating a icon tag
+        // // $img.addClass("circle");
+        // const $ifolder = $("<i>")
+        // // adding a class to the icon tag
+        // $ifolder.addClass("material-icons circle")
+        // // gives text the icon tag
+        // $ifolder.text("folder")
+        // // creating an anchor tag
+        // const $a = $("<a>"); 
+        // // add a class to the anchor tag
+        // $a.addClass("secondary-content")
+        // // gives the anchor tag a link 
+        // $a.attr("href", "#")
+        // // creates another icon tag
+        // const $i = $("<i>")
+        // // gives a class to the second icon tag
+        // $i.addClass("material-icons")
+        // // gives text to the icon tag
+        // $i.text("grade")
+        // // apend the icon tag to the anchor tag
+        // $a.append($i); 
+        // // give userLocation and endLocation tot the p tag
+        // $pTag.html(startLocation + "<br>" + endLocation + "<br>" + key);
+        // // append $ifolder, $span, $a tag
+        // newListItem.append($ifolder, $span, $pTag, $a);
+        // // append new item to order list
+        // $(".collection").append(newListItem); 
+    });
+
+    if (window.firebase) {
+        database.ref().once('value', function(data){
+            console.log(data.val().favorites)
+            const favorites = data.val().favorites
+
+            for (let key in favorites) {
+                console.log(key)
+                console.log(favorites[key])
+                renderFavorites(key, favorites[key].startLocation, favorites[key].endLocation)
+                
+            }
+
+        })
+    }
+    
+    const renderFavorites = (key, startLocation, endLocation) => {
         // create new list item for the unorder list
         const newListItem = $("<li>");
         // add a class to the <li> 
@@ -58,13 +213,12 @@ document.addEventListener('DOMContentLoaded', function() {
         // apend the icon tag to the anchor tag
         $a.append($i); 
         // give userLocation and endLocation tot the p tag
-        $pTag.html(userLocation + "<br>" + endLocation);
+        $pTag.html(startLocation + "<br>" + endLocation + "<br>" + key);
         // append $ifolder, $span, $a tag
         newListItem.append($ifolder, $span, $pTag, $a);
         // append new item to order list
         $(".collection").append(newListItem); 
-    });
-    
+    }
 
 
 
@@ -162,6 +316,7 @@ document.addEventListener('DOMContentLoaded', function() {
         getVehicleOnStop();
 
     });
+    
 
     getIP();
 
